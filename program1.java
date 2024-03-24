e212839dd665ac5303fdc73becd3a706901c84ac
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-24 08:42:36 +0530

    Update and rename README.md to program1.java

---
+++ program1.java
@@ -0,0 +1,11 @@
+# java-day-4
+public class scoreCheck {
+   public static void main(String[]args){
+     int score=75;
+     if(score >=60){
+      System.out.println("You passed.");
+     } else{
+      System.out.println("You failed.");
+     }
+   }
+}

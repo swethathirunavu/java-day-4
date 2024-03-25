6b8876a9974f9a1a32a931d6a8a1e552627db119
Swetha T <145834012+swethathirunavu@users.noreply.github.com>
2024-03-25 19:15:49 +0530

    Update program2.java

--- program2.java
+++ program2.java
@@ -1,1 +1,35 @@
 #program_using_switch
+  public class cybernaut {
+   public static void main(String[]args){
+    int dayNumber = 3; 
+        String dayOfWeek;
+        switch (dayNumber) {
+            case 1:
+                dayOfWeek = "Monday";
+                break;
+            case 2:
+                dayOfWeek = "Tuesday";
+                break;
+            case 3:
+                dayOfWeek = "Wednesday";
+                break;
+            case 4:
+                dayOfWeek = "Thursday";
+                break;
+            case 5:
+                dayOfWeek = "Friday";
+                break;
+            case 6:
+                dayOfWeek = "Saturday";
+                break;
+            case 7:
+                dayOfWeek = "Sunday";
+                break;
+            default:
+                dayOfWeek = "Invalid day number";
+                break;
+        }
+        System.out.println("Day of the week: " + dayOfWeek);
+     
+   }
+}
